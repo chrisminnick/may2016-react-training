d909433cc96b75c6a0a22e8cae0f9aea1c66a197
Chris Minnick <chris@minnick.com>
2016-05-08 15:01:40 -0700

    installed semver

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 const gulp = require("gulp");
 
+//version
+gulp.task('version', function(done) {
+
+    console.log("Checking node version: ");
+    const packageJson = require("./package.json");
+    const expectedVersion	= packageJson.engines.node;
+    const actualVersion	= process.version;
+
+    
+
+
+});
 //default task
 gulp.task('default', function(done) {
   console.log('BUILD OK');

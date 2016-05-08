a325f8937007a78e851024e0ef719aa91ca769e3
Chris Minnick <chris@minnick.com>
2016-05-08 15:10:14 -0700

    configured version task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require("gulp");
+const semver	= require("semver");
 
 //version
 gulp.task('version', function(done) {
@@ -8,10 +9,15 @@ gulp.task('version', function(done) {
     const expectedVersion	= packageJson.engines.node;
     const actualVersion	= process.version;
 
-    
-
-
+    if (semver.neq(expectedVersion,actualVersion)){
+        console.log("Incorrect node version. Expected " +
+            expectedVersion + ". Actual: " + actualVersion);
+        process.exit(1);
+    }
+    done();
 });
+
+
 //default task
 gulp.task('default', function(done) {
   console.log('BUILD OK');
